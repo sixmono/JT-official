eb0d1822414328336510af47871277f378823bb2
sixmono <2043805383@qq.com>
2023-06-28 17:36:54 +0800

    官网v3.4

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,54 +1,34 @@
+import cheerio from "cheerio";
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
   hooks: {
-    generate: {
-      page(page) {
-        const cheerio = require("cheerio");
-        const $ = cheerio.load(page.html, { decodeEntities: false });
- 
-        const attrs = [
-          "data-n-head-ssr",
-          "data-n-head",
-          "data-hid",
-          "data-vue-ssr-id",
-          "data-server-rendered",
-          "data-h-head='1'"
-        ];
- 
-        attrs.forEach(value => {
-          $("*[" + value + "]").removeAttr(value);
-        });
-        
-        page.html = $.html();
-      }
-    }
-  },
-  render:{
-    route(url, result){
-      result.html = result.html.replace(/data-n-head=\"1\"/gi, '')
-    }
- },
+    'render:route': (url, result) => {
+       const ara= cheerio.load(result.html,{decodeEntities: false});
+       ara(`meta`).removeAttr('data-n-head');
+       result.html = ara.html()
+     }
+   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "上海疆通科技有限公司",
     meta: [
       { charset: "utf-8" },
-      { name: "baidu-site-verification", content:"codeva-PT3yV321fk"},
+      { name: "baidu-site-verification", content: "codeva-PT3yV321fk" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-    
-    script:[
+
+    script: [
       {
-        src:"https://mininiu.top/webim/webim.js?id=17044840710842354630",
-        id:"xim_script",
-        charset:"utf-8",
-        type:"text/javascript"
-      }
+        src: "https://mininiu.top/webim/webim.js?id=17044840710842354630",
+        id: "xim_script",
+        charset: "utf-8",
+        type: "text/javascript",
+      },
     ],
   },
 
